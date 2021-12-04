a1429de5da69df29f49326679b896aad7fc64b4c
narenkumarpm2016 <72344948+narenkumarpm2016@users.noreply.github.com>
2021-12-04 20:31:04 +0530

    Delete metadata.sh

--- metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TOKEN=`curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-echo ${TOKEN}
-curl --write-out "\n" --request GET "http://169.254.169.254/latest/" --header "X-aws-ec2-metadata-token: $TOKEN"
-curl --write-out "\n" --request GET "http://169.254.169.254/latest/dynamic/instance-identity/document" --header "X-aws-ec2-metadata-token: $TOKEN"
